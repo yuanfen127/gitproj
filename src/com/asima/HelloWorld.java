fc5740ae1d262b20e718ce7f5372e0d6046de16e
yuanfen127 <yuanfen127@163.com>
2015-12-18 23:19:24 +0800

    添加Until类

--- src/com/asima/HelloWorld.java
+++ src/com/asima/HelloWorld.java
@@ -7,5 +7,9 @@ public class HelloWorld {
 	 */
 	public static void main(String arg[]) {
 		System.out.println("Hello");
+		String prtstr="nic";
+		Until until = new Until();
+		String abc=until.com(prtstr);
+		System.out.println("result=" + abc);
 	}
 }
---
+++ src/com/asima/Until.java
@@ -0,0 +1,9 @@
+package com.asima;
+
+public class Until {
+
+	public String com(String a) {
+		System.out.println(a);
+		return "ok";
+	}
+}
